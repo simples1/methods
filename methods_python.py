e202cbbcd52605d530f7377bcd2c3a84696696d3
simples1 <abdullaaden@outlook.com>
2018-04-02 15:13:23 +0100

    refactor python solution

--- methods_python.py
+++ methods_python.py
@@ -3,9 +3,23 @@ import re
 class TrickyCalculator:
   mapping_correct_keys = {}
 
+
   @classmethod
-  def testing(cls, exp):
-    exp = exp.replace(" ", "").replace("=","")
+  def mapping_correct_keys_meth(cls):
+    eureka = {}
+    for k, v in cls.mapping_correct_keys.items():
+      if str(k) != str(v):
+        print(str(k), " equals " , str(v))
+        eureka[str(k)] = str(v)
+
+    return eureka
+
+  @classmethod
+  def find_missing(cls, exp):
+    exp = exp.replace(" ", "").split("=")
+    result = exp[1]
+    exp = exp[0]
+
     exp = list(exp)
     keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "+", "-", "/", "*"]
 
@@ -39,9 +53,26 @@ class TrickyCalculator:
           test_result = re.sub(r'\-0+', "", test_result)
           test_result = re.sub(r'\/0+', "", test_result)
 
+          #insert leading zero for leading operators
+          if test_result[0] in [ "+", "-","/", "*"]:
+            test_result = "0" + test_result 
+
+          #skip trailing operators
+          if(test_result[len(test_result)-1] in [ "+", "-","/", "*"]):
+            continue
+
           test_result = eval(test_result)
-          print(test_result)
 
+          if (int(test_result) == int(result)):
+            cls.mapping_correct_keys[val] = key
+            break
+    
 
+TrickyCalculator.find_missing("123    = 3")
+TrickyCalculator.find_missing("8423   = 252")
+TrickyCalculator.find_missing("4+4    = 8")
+TrickyCalculator.find_missing("4*7-10 = 417")
+TrickyCalculator.find_missing("9/3    = 3")
+TrickyCalculator.find_missing("42-9   = -36")
 
-TrickyCalculator.testing("12+56 = 3")
+print("These 2 keys have been switched around  ",TrickyCalculator.mapping_correct_keys_meth())
